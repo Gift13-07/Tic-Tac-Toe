7dde8a62388f6df86e28a8446fb3c17659b6361d
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-11 11:57:50 +0100

    git commit

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -14,14 +14,14 @@ for(let box of boxes){
             box.textContent = "X";
             lastValue = "X";
             display.textContent = "Player 2's turn to play";
-            // checkGameStatus();
-            // checkDraw();
+            checkGameStatus();
+            checkDraw();
         } else{
             box.textContent = "O";
             lastValue = "O";
             display.textContent = "Player 1's turn to play";
-            // checkGameStatus();
-            // checkDraw();
+            checkGameStatus();
+            checkDraw();
         }
     });
 }
@@ -30,3 +30,20 @@ let controlBtn = document.getElementById("controlBtn");
 controlBtn.addEventListener("click", () => {
     window.location.href = "Tic-Tac-Toe.html"
 });
+
+
+function checker(first, second, third){
+    if(
+        boxes[first].textContent === "X" &&
+        boxes[second].textContent === "X" &&
+        boxes[third].textContent === "X" 
+    ) {
+        disabledButtonsWhenGameIsWon("Player 1");
+    } else if (
+        boxes[first].textContent === "O" &&
+        boxes[second].textContent === "O" &&
+        boxes[third].textContent === "O"
+    ) {
+        disabledButtonsWhenGameIsWon("Player 2");
+    }
+}
